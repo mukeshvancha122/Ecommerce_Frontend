5de7c597af44f34d6624a274ef676f8067fbf448
Mukesh Reddy <mukeshvancha122@gmail.com>
2025-10-24 20:36:05 -0400

    State management configuration using redux

---
+++ src/reducer.js
@@ -0,0 +1,54 @@
+export const initialState = {
+  cart: [],
+  user: null,
+};
+
+export const getBasketTotal = (cart) =>
+  cart?.reduce((amount, item) => item.price + amount, 0);
+
+const reducer = (state, action) => {
+  console.log(action);
+  switch (action.type) {
+    case "ADD_TO_CART":
+      return {
+        ...state,
+        cart: [...state.cart, action.item],
+      };
+
+    case "EMPTY_CART":
+      return {
+        ...state,
+        cart: [],
+      };
+
+    case "REMOVE_FROM_CART":
+      const index = state.cart.findIndex(
+        (cartItem) => cartItem.id === action.id
+      );
+      let newCart = [...state.cart];
+
+      if (index >= 0) {
+        newCart.splice(index, 1);
+      } else {
+        console.warn(
+          `Cannot remove the product (id: ${action.id}) Not available in the cart!`
+        );
+      }
+
+      return {
+        ...state,
+        cart: newCart,
+      };
+
+    case "SET_USER":
+      return {
+        ...state,
+        user: action.user,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default reducer;
