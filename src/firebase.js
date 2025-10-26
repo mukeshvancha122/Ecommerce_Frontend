35a173831e28f0fda06ed9b6e5c4dfcce38e0f98
Mukesh Reddy <mukeshvancha122@gmail.com>
2025-10-26 17:28:26 -0400

    firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,12 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBk3jVmeG97VsrfwJBGkr6ad22ao-GpDX8",
-  authDomain: "amaznclone-v1.firebaseapp.com",
-  projectId: "amaznclone-v1",
-  storageBucket: "amaznclone-v1.appspot.com",
-  messagingSenderId: "271279434053",
-  appId: "1:271279434053:web:da06abdd2b4c11403e362c",
-  measurementId: "G-41JSRQJFDW",
+  apiKey: "AIzaSyDEJHpwLIFri_3vqOQyBA-C6WgbL7BjwNU",
+  authDomain: "ecommerce-clone-dc.firebaseapp.com",
+  projectId: "ecommerce-clone-dc",
+  storageBucket: "ecommerce-clone-dc.firebasestorage.app",
+  messagingSenderId: "610887915093",
+  appId: "1:610887915093:web:3afdc8ee4d66bb53f266dc"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
